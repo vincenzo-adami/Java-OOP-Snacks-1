cf9d567a3fbd9ef738c09457ecd952c3027059fe
vincenzo-adami <51445073+vincenzo-adami@users.noreply.github.com>
2024-09-27 11:39:32 +0200

    ADD: commento con traccia esercizio a linea 1

--- org/lessons/java/snack1/Studente.java
+++ org/lessons/java/snack1/Studente.java
@@ -1,3 +1,5 @@
+// Scrivi una classe Studente con i seguenti attributi:nome,cognome,età.Implementa un costruttore che accetta tre parametri per inizializzare questi attributi.Aggiungi un metodo che restituisca una stringa concatenata dei valori(es.Anselmo Bitta,24 anni).
+
 public class Studente {
 
   private String nome;
